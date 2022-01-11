0e0e70a88d6d1204f65c7479d83338c2a9c5d275
Jjalcsan <jjalcsan@gmail.com>
2022-01-11 14:25:28 +0100

    11Jan

--- servers/src/app/app.module.ts
+++ servers/src/app/app.module.ts
@@ -9,11 +9,19 @@ import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users/users.component';
 import { ServersComponent } from './servers/servers/servers.component';
 import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from './users/user/user.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServerComponent } from './servers/server/server.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'users', component: UsersComponent},
-  { path: 'servers', component: ServersComponent}
+  { path: 'servers', component: ServersComponent},
+  { path: 'users', component: UsersComponent},
+  { path: 'users', component: UserComponent},
+  { path: 'users/:id/:name', component: UserComponent},
+  { path: 'servers/:id/edit', component: EditServerComponent},
+  { path: 'servers/:id', component: ServerComponent}
 ];
 
 
